4c2ab16e708fa44808565ec86cf575fd8eb9c364
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-04-16 11:19:46 +0530

    ensure proper loading of external svg.js files
    Signed-off-by: M-DEV-1 <mahadevankizhakkedathu@gmail.com>

--- site/gatsby-browser.js
+++ site/gatsby-browser.js
@@ -3,7 +3,7 @@ export const onInitialClientRender = () => {
     const loadScript = (src) => {
       const script = document.createElement('script');
       script.src = src;
-      script.async = true;
+      script.async = false;
       document.body.appendChild(script);
     };
   
